e8c8c482696c9f984f619055442c14b52b2b88c2
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 20:51:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|h,k| h[k] = Array.new}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+   def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
