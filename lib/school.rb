61167028aeb0093e0e7ee337ef9582331103a16a
Cheng Vue <cvu3@ucsc.edu>
2018-11-24 12:13:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+
+    roster.each do |grade, names|
+      sorted_roster[grade] = names.sort
+    end
+
+    return sorted_roster
+  end
+
+  def add_student(student, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+      roster[grade] << student
+    else
+      roster[grade] << student
+    end
+  end
+end
