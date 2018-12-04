397cef5766006c4cc404bb8e7aa26af52da7eb96
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-04 17:49:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+#initialization method for the class
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+#this adds a student to the hash
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+        roster[grade] << student
+    else
+        roster[grade] = []
+        roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each_value{|value| value.sort!}
+  end
+
+end
