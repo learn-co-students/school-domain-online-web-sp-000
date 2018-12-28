44a6de19a6e098e1e338c6eee2defd54dbab149a
Cody Rickman <cody@creativemindstudios.com>
2018-12-28 22:35:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  def initialize(school_name)
+    @this_school_name = school_name
+    @this_roster = {}
+  end
+  def add_student(name, grade)
+    if (!(@this_roster.key?(grade)))
+      @this_roster[grade] = []
+    end
+    @this_roster[grade] << name
+  end
+  def roster
+    @this_roster
+  end
+  def grade(grade)
+    @this_roster[grade]
+  end
+  def sort
+    new_hash = Hash[@this_roster.sort]
+    new_hash.each do |key, names|
+      new_hash[key] = names.sort
+    end
+  end
+end
