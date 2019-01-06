811f9eaaf6b254ab9730a054c6a20a439f063b9d
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-06 01:06:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
