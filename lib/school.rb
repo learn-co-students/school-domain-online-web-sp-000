35f8b21b5f92fad11ca9e483c9b2f04ebac879dc
Karl Benbeneck <kbenbeneck@gmail.com>
2019-01-09 16:22:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+  def add_student(name, grade)
+    @roster[grade] = [] if roster[grade].nil?
+    @roster[grade] << name
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |key,values|
+      values.sort!
+    end
+    @roster
+  end
+end
