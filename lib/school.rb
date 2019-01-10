f3b61650c821914885f5edb19fd1ba38693c5405
Anup Khadka <akhadka.dev@gmail.com>
2019-01-10 03:35:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.keys.include?(grade)
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_hash = {}
+    @roster.each do |grade, student_array|
+        sorted_hash[grade] = student_array.sort
+    end
+    sorted_hash
+  end
+
+end
