2fb7fe3729b6e4afca4f1b9ebe330b7b6b085020
Dr. XyClo <sdcarmody@gmail.com>
2019-01-14 00:45:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -16,14 +16,35 @@ class School
     end 
   end
   
-def grade(grade)
-  @roster[grade]
-end 
+  def grade(grade)
+    @roster[grade]
+  end 
+
+  def sort
+    @roster.each do |grade_hash, students_array|
+      students_array.sort
+    end
+  end
+end
+
 
 =begin 
+describe "#sort" do
+    it 'is able to sort the students' do
+      @school.add_student("Homer Simpson", 9)
+      @school.add_student("Bart Simpson", 9)
+      @school.add_student("Avi Flombaum", 10)
+      @school.add_student("Jeff Baird", 10)
+      @school.add_student("Blake Johnson", 7)
+      @school.add_student("Jack Bauer", 7)
 
+      # key order does not matter; this is testing that the students in each respective value are in alphabetical order
+      expect(@school.sort).to eq({7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]})
+    end
+  end
+end
 =end 
 
-end
+
 
 
