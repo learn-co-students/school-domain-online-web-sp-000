bbeab90078c1b964def0af2ad2eae1a5591aaa5c
Dr. XyClo <sdcarmody@gmail.com>
2019-01-14 01:15:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,52 +1,35 @@
+require 'pry'  ## require pry outside class and calling binding.pry inside works
+
 class School
   
-   attr_accessor :name, :roster 
+   attr_accessor :name, :roster ## sets setter and getter methods, still not fluent in following the logic and flow, when not present nothing works
   
-  def initialize(name)
+  def initialize(name)  ## when new instance is called, the instance is passed with the name and given an empty roster hash 
     @name = name
     @roster = {}
   end 
   
-  def add_student(student_name, student_grade)
-    if !@roster.include?(student_grade)
-      @roster[student_grade] = []
-      @roster[student_grade] << student_name 
-    else
-      @roster[student_grade] << student_name
+  def add_student(student_name, student_grade)  ## method that adds key-value, student_grade and student_name to the roster hash.  Compiles @roster for                                                    school
+  
+    if !@roster.include?(student_grade)         ## checks @roster hash for existence of student_grade key 
+      @roster[student_grade] = []               ## if student_grade doesn't exist adds student_grade key with empty value/student_name array 
+      @roster[student_grade] << student_name    ## pairs the student_name value to the student_grade key inside the roster hash 
+      binding.pry                               ## binding.pry intercepts method here 
+    else                                        ## if student_grade key already exists
+      @roster[student_grade] << student_name    ## pairs the student_name value to the student_grade key inside the roster hash
     end 
   end
   
-  def grade(grade)
-    @roster[grade]
+  def grade(grade)  ## grade method accepts the grade and returns a complete roster for that grade
+    @roster[grade]  ## returns a school instance's student value array for the grade key passed to it  
   end 
 
-  def sort
-    #@roster_sort = {}
-    @roster.each do |grade_hash, students_array|
-     # grade_hash => students_array # .sort
-     # @roster_sort << [grade_hash] = 
-      @roster[grade_hash] = students_array.sort
+  def sort                                        ## sort method sorts the student value array alphabetically a->z and returns the sorted hash 
+    @roster.each do |grade_hash, students_array|  ## each iterator passes the grade key and student value to the code below
+      @roster[grade_hash] = students_array.sort   ## the alphabetically sorted student array values are added to the pair grade keys creating a new student                                               value sorted @roster hash 
     end
   end
 end
-
-
-=begin 
-describe "#sort" do
-    it 'is able to sort the students' do
-      @school.add_student("Homer Simpson", 9)
-      @school.add_student("Bart Simpson", 9)
-      @school.add_student("Avi Flombaum", 10)
-      @school.add_student("Jeff Baird", 10)
-      @school.add_student("Blake Johnson", 7)
-      @school.add_student("Jack Bauer", 7)
-
-      # key order does not matter; this is testing that the students in each respective value are in alphabetical order
-      expect(@school.sort).to eq({7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]})
-    end
-  end
-end
-=end 
 
 
 
