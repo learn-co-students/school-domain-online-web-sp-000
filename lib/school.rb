00768aa6b1c9f50e6edf98bab48f9474f1796bf6
Dr. XyClo <sdcarmody@gmail.com>
2019-01-14 00:43:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  
+   attr_accessor :name, :roster 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student_name, student_grade)
+    if !@roster.include?(student_grade)
+      @roster[student_grade] = []
+      @roster[student_grade] << student_name 
+    else
+      @roster[student_grade] << student_name
+    end 
+  end
+  
+def grade(grade)
+  @roster[grade]
+end 
+
+=begin 
+
+=end 
+
+end
+
+
