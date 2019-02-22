54062b7a692891e2765be4d88c444e9d08bc5345
Wendy Hamilton <wdhamilton117@gmail.com>
2019-02-22 15:54:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+  attr_accessor :roster, :name, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new{ |hash,key| hash[key] = [] } # intiatilizing an emtpty array with a default empty hash
+  end
+
+  def add_student(name, grade)
+      @roster[grade] << name
+    end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
