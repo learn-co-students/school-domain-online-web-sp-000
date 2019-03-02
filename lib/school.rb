0121e43ce64b7fdb6c63fcf50862be03b57a8eb6
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-02 08:55:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+def initialize(school_name)
+@school_name=school_name
+@roster={}
+end
+
+def roster
+  @roster
+end
+
+  def add_student(student,grade)
+    if(@roster.has_key?(grade))
+      @roster[grade]<< student
+    else
+    @roster[grade]=[]
+    @roster[grade]<< student
+  end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade,student|
+      @roster[grade]=student.sort
+    end
+  end
+
+  
+end
+
