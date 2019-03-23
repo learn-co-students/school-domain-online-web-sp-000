1fdc7c66eef62988afaebceaf1d7add1b9c1783e
Sherief Farid <sherief@farid.cloud>
2019-03-23 05:28:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+
+require 'pry'
+
+class School
+  attr_accessor
+  attr_reader :roster
+  attr_writer
+
+
+  def initialize(roster)
+    @roster = roster
+    @roster = {}
+
+  end
+
+  def add_student= (student,grade)
+    if !(ROSTER.include?(grade))
+      ROSTER[grade] = []
+    end
+    ROSTER[grade] << student
+    @student = student
+    @grade = grade
+  end
+
+
+end  #end of class
