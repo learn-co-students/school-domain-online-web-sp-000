b87de88c042bd9145db2832a596765ee95353f1a
Christopher Luna <chris@illusionlayervfx.com>
2019-04-14 03:26:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :name
+  attr_reader :roster
+
+def initialize(name)
+  @roster = {}
+end
+
+def add_student(name, grade)
+  roster[grade] ||= []
+  roster[grade] << name
+end
+
+def grade(grade)
+  roster.each do |key, value|
+    if key == grade
+       return value
+    end
+  end
+end
+
+def sort
+  sorted_hash = {}
+    roster.collect do |key, value|
+      sorted_hash[key] = value.sort
+    end
+  sorted_hash
+end
+
+end
