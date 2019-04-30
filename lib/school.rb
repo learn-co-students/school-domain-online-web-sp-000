b537a0549f62add1daac9983d17cbeba69299d8c
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-30 02:08:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+ 
+attr_accessor :roster 
+attr_reader :student, :grade
+ 
+ROSTER = []
+
+def initialize(roster)
+  @roster = ROSTER
+ end
+end
+
+def add_student(student, grade)
+  ROSTER << student
+ end
