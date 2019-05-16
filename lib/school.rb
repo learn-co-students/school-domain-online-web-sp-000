4239828bc1dd8f088e4dc78f25e43ebb0cf9bf9a
Alan Krajina <alankrajina88@gmail.com>
2019-05-16 19:37:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
 # code here!
+
+class School
+  attr_accessor :roster, :name  # @ ATRIBUTE accessors
+
+
+  def initialize(name) # initializes when .new called
+    @name=name
+    @roster={}
+  end
+
+  def add_student(name, grade) # adds name VALUE to hash roster    
+
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
+  
+  def grade(grade)    # iterates through roster hash, compares KEY and                       grade, and if its the same RETURNS associated                       VALUE
+    roster.collect do |x,y|
+    if x == grade
+      return y
+    end
+  end
+  end
+  
+  def sort       # iterates through roster hash and .sorts values and                   assigns to a new hash
+   nu_hash = {}
+   roster.each do |x, y| 
+     nu_hash[x] = y.sort 
+   end 
+   nu_hash
+  end 
+  
+end
+
+
+
+
