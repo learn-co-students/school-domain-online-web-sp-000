324327d2b9808354c95f035ef674b85873e35ca3
Abagail Hudson <abagailanne@gmail.com>
2019-05-19 18:27:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade) #grade is like 6th grade, 7th 8th etc
+    #roster[grade] = [] unless roster[grade]
+    # takes into account for empty hash
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(level)
+    roster[level]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |name, grade|
+      sorted[name] = grade.sort
+    end
+    sorted
+  end
+end
