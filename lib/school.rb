92b5db0d4077b5f3dc4c4e899f5e63f98deec229
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-21 18:45:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,36 @@
 # code here!
 class School
-attr_accessor :name, :roster
+  attr_reader :name, :roster
 
-def initialize(name)
-  @name
-  @roster = {}
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+  def add_student(name, grade)
+
+    self.roster[grade] ||= []
+    #if self.roster[grade] already exists, leave it alone
+    #else set self.roster[grade] = []
+    self.roster[grade] << name
+
+    #if @roster.has_key?(grade)
+    #  @roster[grade] << name
+    #else
+    #  @roster[grade] = [name]
+  #end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+def sort
+  self.roster.each do |grade, name_array|
+    self.roster[grade]= name_array.sort
+  end
 end
 
-def add_student(student_name,grade)
-  roster[grade] ||= []
-  roster[grade] << student_name
-end
 
-def grade(student_grade)
-  sorted = {}
-  roster.each do |grade, students|
-    sorted[grade] = student.sort
-end
-sorted
-end
 
 end
