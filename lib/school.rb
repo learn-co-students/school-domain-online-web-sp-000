cef0f1a32d59c11b9f80052c3d5231d747988831
Ranika Williams <rnkprice07@gmail.com>
2019-05-23 18:50:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,26 @@
 class School
+  attr_accessor :name, :roster
 
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
 
-  def roster
-    @roster
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
 
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
+  def grade(student_grade)
+    roster[student_grade]
   end
 
-  def grade(number)
-    @roster[number]
-  end
-
+  # this method should arrange the students in each grade by alphabetical order
   def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
     end
+    sorted
   end
 end
