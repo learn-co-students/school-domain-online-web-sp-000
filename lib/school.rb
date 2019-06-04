116557dbe161e334428303f2c0110ae3d20f04dc
Marc Poppy <poppy.marc@gmail.com>
2019-06-04 00:23:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+
+class School
+  # attr_accessor :length
+  attr_reader :name, :roster
+
+  #SCHOOL_ROSTER = {}
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort()
+    @roster.map { |k,v| [k,v.sort] }.to_h
+  end
+end
