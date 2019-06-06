dda3cbbb7cf179d3e132425623d79f6c244b0b70
Lukie Kang <lukiek+f@gmail.com>
2019-06-06 06:11:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
 # code here!
+
+class School
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+attr_reader :name
+
+def roster
+  @roster
+end
+
+
+def add_student(student_name, grade)
+  if @roster.key?(grade)
+    puts "Grade Exists"
+  else
+  @roster[grade] = []
+  end
+  @roster[grade] << student_name
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, arr|
+    arr.sort! do |a,b|
+      a <=> b
+    end
+    puts "hello Longsorted"
+    puts arr
+
+  end
+end
+
+
+end
