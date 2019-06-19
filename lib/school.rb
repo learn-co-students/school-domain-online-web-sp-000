9d55e77035a6a284542b4a6b6401af87d628e1e3
juan siu <juan.siu@gmail.com>
2019-06-19 20:49:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,11 +10,28 @@ class School
   end
   
   def add_student(student , grade)
-    if @roster == {}
-       @roster[grade] = [student]
+    if @roster.key?(grade)
+       @roster[grade] << student
    
     else
-      @roster[grade] << student
+      @roster[grade] = [student]
     end
   end
+  
+  def grade(grade)
+      @roster[grade]
+  end
+
+  def sort
+ 
+    sorted_hash = {}
+    final_hash = {}
+      @roster.sort.map do |key,value|
+      sorted_hash = {key => value.sort}
+     final_hash.merge!(sorted_hash)
+    end
+    return final_hash
+  end
+
 end
+  
