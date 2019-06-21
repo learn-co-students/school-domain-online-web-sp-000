912558880ac8d823fde1499cecb2a940fa65475b
Eric Clark <eclark0013@gmail.com>
2019-06-21 15:55:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+#  attr_accessor :roster
+  attr_reader :roster
+
+  def initialize(roster)
+    @roster={}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]==nil
+      @roster[grade]=[name]
+    else
+      @roster[grade]<<name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      @roster[key]=value.sort
+    end
+    @roster.sort.to_h
+  end
+
+end
