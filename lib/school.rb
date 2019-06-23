a6f2c8ae47a7b11100ebcaf9fea17c6f4857b5fa
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-23 01:00:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  
+  ROSTER = [ ]
+  
+  attr_reader :roster, :add_student
+  
+  def initialize(roster)
+    @roster = ROSTER
+  end
+  
+  def add_student(name,grade)
+    @add_student = add_student
+  end
