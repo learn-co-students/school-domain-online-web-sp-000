9d0f2bc61ffa0fe189a05c693d6acd88ce58f3c9
Connor Cowan <ccowan003@comcast.net>
2019-06-24 22:38:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
-# code here!
+class School          # School class refers to a "model" in the domain model context.
+  attr_accessor = :school :roster :grade
+  attr_reader = :add
+
+
+  def initialized(add_student)
+    @add_student = add_student
+  end
+end
