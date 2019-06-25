85592c699aae5c883e44e2b839e91a1d6909693e
Connor Cowan <ccowan003@comcast.net>
2019-06-25 01:50:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,25 @@
-class School                         # School class refers to a "model" in the domain model context.
-  attr_accessor = :name, :roster
+class School                            #School class refers to a "model" in the domain model context.
+  attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
-    @roster = {}              #@roster our hash
+    @roster = {}
   end
 
-  def add_student(name, grade)
-    roster[grade] ||= {}                   #Conditional assignment operator: is equivalent to a||a=b
-    roster[grade] = name
+  def add_student(student_name, grade)
+    roster[grade] ||= []               #Conditional assignment operator: is equivalent to a||a=b
+    roster[grade] << student_name       
   end
 
-  def grade(grade)
-  roster[grade]
-end
+  def grade(student_grade)
+    roster[student_grade]
+  end
 
-  # this method should arrange the students in each grade by alphabetical order
   def sort
-   sorted = {}
-   roster.each do |grade, students|
-     sorted[grade] = students.sort          #condition which sorts our students alphabetically by the #sort method
-   end
-   return sorted
- 
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort                             #condition which sorts our students alphabetically
+    end
+    return sorted
+  end
 end
-
-school = School.new("Bayside High School")
