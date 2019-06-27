cf8cad39651e5895b39e0208e0a8e86044e46620
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-27 22:22:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,34 +1,28 @@
 require 'pry'
 class School
+attr_reader :school, :roster
 
-def initialize(school_name)
-  @school = school_name
-end
-def school
-  @school
+def initialize(school)
+  @school = school
+  @roster = {}
 end
 
-  # def roster=(roster)
-  #   @roster = {}
+def add_student(student, grade)
+  @roster[grade] = []
+  @roster[grade] << student
+  # # @roster[grade] = student
   # end
-  def roster
-    @roster = {}
-  end
-
-def add_student (grade, student)
-   @roster[grade] = [student]
-  # @roster[grade] << student
 end
 
 
-  def length(input)
-    length = []
-    @roster.each do |grade, students|
-       students.each do |student|
-         length << student
-       end
-    end
-   length.length
-end
+#   def length(input)
+#     length = []
+#     @roster.each do |grade, students|
+#        students.each do |student|
+#          length << student
+#        end
+#     end
+#    length.length
+# end
 
 end
