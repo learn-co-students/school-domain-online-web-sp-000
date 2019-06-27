73452aeb27c02ab9c7a5fca31a53eb30aa771cf4
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-27 21:55:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+class School
+
+def initialize(school_name)
+  @school = school_name
+end
+def school
+  @school
+end
+
+  # def roster=(roster)
+  #   @roster = {}
+  # end
+  def roster
+    @roster = {}
+  end
+
+def add_student (grade, student)
+  @roster[grade] = []
+  @roster[grade] << student
+end
+
+
+  def length(input)
+    length = []
+    @roster.each do |grade, students|
+       students.each do |student|
+         length << student
+       end
+    end
+   length.length
+end
+
+end
