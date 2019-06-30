87bd90bf2d58b14e1c44eb74f242edf7f7422b7a
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-30 20:08:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require "pry"
+
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  attr_accessor :name, :roster
+  
+  def add_student(student, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade]=[]
+    @roster[grade] << student
+  end
+end
+
+def grade(grade)
+  return @roster[grade]
+end
+
+def sort
+  new_hash = {}
+  @roster.each do |grade, students|
+     new_hash[grade]=students.sort
+  end
+  new_hash
+end
+
+    
+  
+  
+end
