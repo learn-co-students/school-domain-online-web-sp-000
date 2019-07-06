382161592e533d6035d8c0936860eebac83e1148
Bernadette Davis <bsd231@nyu.edu>
2019-07-06 18:31:48 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+
+class School
+
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def roster=(roster)
+  @roster = roster
+end
+
+def add_student(names, grade)
+  if !@roster.include?(grade)
+    @roster[grade] = []
+  end
+
+  @roster[grade] << names
+
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each {|n, array1|
+    #@roster.keys.sort
+    array1.sort!
+  }
+
+end
+
+
+end
