7d0d8fefddd669ee65c1611d57bf9ae29d2f009c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-01 22:05:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,36 +1,25 @@
 class School
+  attr_accessor :name, :roster
 
-  attr_accessor :school
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
 
-  def initialize(roster)
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(student_name, grade)
-    if roster.has_key?(grade)
-      roster[grade] << student_name
-    else
-      roster[grade] = []
+    def add_student(student_name, grade)
+      roster[grade] ||= []
       roster[grade] << student_name
     end
-      roster
-  end
 
-  def grade(num)
-    if roster.has_key?(num)
-      roster[num]
+    def grade(student_grade)
+      roster[student_grade]
+    end
+
+    def sort
+      sorted = {}
+      roster.each do |grade, students|
+        sorted[grade] = students.sort
+      end
+      sorted
     end
   end
-
-  def sort
-    sorted_hash = {}
-      roster.each do |key, value|
-        sorted_hash[key] = value.sort
-    end
-    sorted_hash
-  end
-end
