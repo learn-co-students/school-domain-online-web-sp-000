f5fd5a195bc7763adda502c8318052a00a06c6fc
Brian Pompey <brianthescholar@gmail.com>
2019-08-15 02:22:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+
+  def add_student(grade, name)
+    roster[name] ||= []
+    roster[name]<< grade
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+  #  binding.pry
+    hash = {}
+    roster.each do |key, values|
+      roster[key] = values.sort
+    end
+#  hash << roster
+
+  end
+
+end
