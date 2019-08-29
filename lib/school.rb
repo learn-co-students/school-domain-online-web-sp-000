5b7d3488b1c3cfce187118b131c707d53c69b111
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 19:26:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def roster 
+   @roster
+  end 
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade) == true 
+      @roster[grade] << name 
+    else 
+      @roster[grade] = []
+      @roster[grade] << name 
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    new_roster = {}
+    new_roster = @roster.each do |grade, name| 
+      name.sort
+    end 
+    binding.pry
+    return @new_roster
+  end 
+end
