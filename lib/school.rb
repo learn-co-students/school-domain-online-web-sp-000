ebe3d3a412138582563e9c36fb2bc98891f24f42
Haim D <haim11293@aol.com>
2019-09-17 19:57:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,14 @@ class School
     roster[grade] << name
   end
 
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+
 end
