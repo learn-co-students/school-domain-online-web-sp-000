ca002b2678913c94004bb7a94b599fe6473b2775
Blythe Pollard <blytheepollard@gmail.com>
2019-09-28 21:17:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 class School
-  attr_accessor :name, :roster
+  attr_accessor :name, :roster, :grade
   
   def initialize(name)
     @name = name
@@ -8,10 +8,15 @@ end
   
   def add_student(name, grade)
     roster = @roster
-    roster << []
+    if roster.include?(grade)
+     roster[grade] << name
+    else roster[grade] = []
+      roster[grade] << name
+    end
 end
 
-
+  def grade(grade)
+    
 end
 
 
