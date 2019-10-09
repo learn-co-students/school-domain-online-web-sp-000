9e5c5d8596aaf2b5f8cb97018e7a542dd44c879d
Jack OMalley <jfo.omalley@gmail.com>
2019-10-09 21:15:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    return_hash = {}
+    @roster.each do |grade, student|
+      return_hash[grade] = student.sort
+    end
+    return_hash
+  end
+end
