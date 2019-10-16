a92810680a14776e71e53d91d9560dd9858a4dc6
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-16 04:14:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    student_list = @roster.values
+    student_list.each do |list|
+      list.sort!
+    end
+    @roster
+  end
+end
