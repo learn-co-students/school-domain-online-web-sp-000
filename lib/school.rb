da2968bcfddb42f6578562f74663421cfa58d7a3
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-31 14:03:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_accessor :name, :roster 
+  
+  def intialize(name)
+    @name = name
+    @roster = {}
+  end   
+  
+  def add_student=(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+  end   
+end   
