f7ac29c25434d782ab7713a9f991f6992b1879b5
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 21:06:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] << student
+  end
+
+  def grade(grade_num)
+    roster[grade_num]
+  end
+
+  def sort
+    roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
