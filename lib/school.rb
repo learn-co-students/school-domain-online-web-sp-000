3abfce5ea3f898f5d75d479d4db2b28a08bea53e
Otto Cedeno <otto@ottostacos.com>
2019-11-28 04:19:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(student, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = Array.new(1, student)
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade, students| students.sort!}
+  end
+end
+
+binding.pry
