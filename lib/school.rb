980da8da40095888efdafc8a7c54f9a2800ae6b7
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-11 23:17:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+
+  def grade(grade)
+    roster.detect do |x, y|
+      if x == grade
+        return y
+      end
+    end
+  end
+
+  def sort
+  nu_hash = {}
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
+  nu_hash
+end
+
+end
