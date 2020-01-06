2837d418c610264d438008414eef6be6a7d1e588
Will Carter <will.carter@fergusllc.com>
2020-01-06 16:19:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    @roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
+
+school = School.new("Test School")
+# school.add_student("Avi Flombaum", 10)
+# school.add_student("Jeff Baird", 10)
+# school.add_student("Blake Johnson", 7)
+# p school.grade(10)
+
+school.add_student("Homer Simpson", 9)
+school.add_student("Bart Simpson", 9)
+school.add_student("Avi Flombaum", 10)
+school.add_student("Jeff Baird", 10)
+school.add_student("Blake Johnson", 7)
+school.add_student("Jack Bauer", 7)
+p school.sort
