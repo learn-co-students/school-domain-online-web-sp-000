5c3721b9c8950ed3eab541dbd6502443e16390ea
Will Carter <will.carter@fergusllc.com>
2020-01-06 19:54:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -8,19 +8,19 @@ class School
   end
 
   def add_student(student, grade)
-    if @roster[grade] == nil
-      @roster[grade] = []
+    if roster[grade] == nil
+      roster[grade] = []
     end
-    @roster[grade] << student
+    roster[grade] << student
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
     sorted = {}
-    @roster.each do |grade, students|
+    roster.each do |grade, students|
       sorted[grade] = students.sort
     end
     sorted
