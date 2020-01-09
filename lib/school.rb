c74f9cf90a56c98c7b45b175a7e0805a809165d6
Aoune Syed <aoune007@gmail.com>
2020-01-09 05:54:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def name
+    @name = name
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+
+    if roster[grade]
+      return student_name
+    else
+      roster[grade] << student_name
+    end
+
+
+  end
+  #def add_student(student_name, grade)
+    #roster[grade] ||= []
+    #roster[grade] << student_name
+#  end
+
+  #def grade(student_grade)
+  #  roster[student_grade]
+#  end
+
+
+end
