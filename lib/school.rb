90bb3caa211aeb670abedbf3fb7096132bb2c651
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-10 19:24:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade].push(name)
+    else 
+      @roster[grade] = []
+      @roster[grade].push(name)
+    end
+  end
+  
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+    @roster
+  end
+  
+end
