c8b0870480e9ec57d130a16069371228763ee58e
Amy Fong <amyhfong@gmail.com>
2020-01-10 17:43:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+  ROSTER = {}
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    @roster = ROSTER
+  end
+
+  def add_student(name, grade)
+    if ROSTER.key?(grade)
+        ROSTER[grade] << name
+      else
+        ROSTER[grade] = [name]
+      end
+  end
+
+end
