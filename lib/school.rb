67937f68a2e6aba54fc40d7f745bafd8b385b133
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-13 17:03:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,3 @@
 # code here!
 require 'pry'
 
-class School
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
-    end
-  end
-
-  def grade(grade)
-    @roster[grade]
-  end
-  
-  def sort 
-    sorted_hash = {}
-    @roster.each do |k, v| 
-      sorted_hash[k] = v.sort 
-    end
-    sorted_hash
-  end 
-end 
