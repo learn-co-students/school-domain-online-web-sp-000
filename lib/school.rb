c92181d0602651e69b57288722d94bedc9fc239a
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-13 17:12:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,17 @@
 # code here!
 require 'pry'
 
+class School
+  def initialize(school) 
+    @school = school 
+    @roster = {} 
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    
+  end 
+end 
