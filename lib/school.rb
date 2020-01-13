6ded77fa6c55fc90f9556293c6bfccfafd80c4b6
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-13 23:24:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,18 @@ class School
   end 
   
   def add_student(name, grade)
-    
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    sorted_hash = {}
+    @roster.each {|k, v| sorted_hash[k] = v.sort}
+    sorted_hash
   end 
 end 
+
