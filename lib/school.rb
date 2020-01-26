e01d05a64e767223a51d0ab806392b659f8c9051
Doak Covington <doakcovington@protonmail.com>
2020-01-26 03:50:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+#school class
+class School
+  attr_accessor :grade
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    if roster[grade]
+      roster[grade] << name
+    else
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, name|
+      sorted[grade]=name.sort 
+    end
+    sorted
+  end
+end #end of school class
