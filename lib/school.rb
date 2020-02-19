e82e17a8a714b430667bc805e1a303bff7e0c5a4
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-19 19:19:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+
+def initialize (name)
+@name = name
+
+@roster = {}
+end
+
+
+
+def roster
+  @roster
+end
+
+def add_student(students_name, grade)
+if @roster.has_key?(grade) == false
+@roster[grade] = []
+@roster[grade] << students_name
+else
+  @roster[grade] << students_name
+
+end
+end
+
+def grade(num)
+return @roster[num]
+end
+
+def sort
+  hash = @roster.each {|keys, ary| @roster[keys] = ary.sort}
+
+hash
+end
+
+end
