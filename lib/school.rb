3b1fddd65ecb71a1e57052edc795867f421185d7
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-19 09:51:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(student, grade)
+    @roster[grade] = [] unless @roster.has_key?(grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    @roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
