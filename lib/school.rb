26679f0eb2ca0b9e24a212e974a9ba959187dec5
Seth Todd <seth.todd@outlook.com>
2020-02-23 06:35:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :roster, :school_name
+  
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster.include?(grade)
+        @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    new_hash = {}
+    @roster.each do |grade, student|
+      new_hash[grade] = student.sort
+    end
+    new_hash
+  end
+end
+    
+    
+    
