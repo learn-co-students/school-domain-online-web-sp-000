52f75605ccf97d33a243b79ffea32939fa7d0849
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-10 20:47:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_reader  :school_name, :roster
+    def initialize(school_name)
+          @school_name = school_name
+            roster = []
+            @roster = roster
+    end
+
+    def add_student=(student)
+     @student = student
+     @roster << student
+    end
+    def add_student
+         @roster
+    end
+  end
+
+end
