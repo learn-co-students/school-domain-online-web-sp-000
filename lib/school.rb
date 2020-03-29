ffcb66ae23d154fe264744242d77938c5ea96b6a
Anna Guerrero <acg2176@columbia.edu>
2020-03-29 16:56:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if self.roster.key?(grade)
+      self.roster[grade] << name
+    else
+      self.roster[grade] = []
+      self.roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, names|
+      names.sort!
+    end
+    self.roster
+  end
+
+end
