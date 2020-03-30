5d343b3199f187bebc8c65323a6420de250121d4
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-30 15:36:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  # add a student to the grade's list
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  # return the array of all students
+  # that are in the given grade
+  def grade(grade)
+    @roster[grade]
+  end
+
+  # alphabetically sort students in each grade
+  def sort
+    return_hash = {}
+    @roster.each  {|grade, students| return_hash[grade] = students.sort}
+    return_hash
+  end
+
+end
