95c9946a00071c9fd23e758d5bf655725caed133
Micah McCallum <micahmccallum@outlook.com>
2020-03-31 03:04:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  attr_accessor :student, :grade, :roster
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+      roster[grade].push(student)
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+
+  def sort
+    # tempArray = []
+    roster.each do |grade, studentArray|
+      roster[grade] = studentArray.sort
+    end
+  end
+end
