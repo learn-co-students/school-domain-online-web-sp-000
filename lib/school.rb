7d33b09e2382412d8a7b8032e39ac8d51a4f23ec
Micah McCallum <micahmccallum@outlook.com>
2020-03-31 18:38:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,12 +10,8 @@ class School
   end
 
   def add_student(student, grade)
-    if roster.has_key?(grade)
-      roster[grade].push(student)
-    else
-      roster[grade] = []
+      roster[grade] ||= []
       roster[grade] << student
-    end
   end
 
   def grade(grade)
