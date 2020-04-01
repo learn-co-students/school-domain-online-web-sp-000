2c140f0f3d4e6ae42acf7c640afe95f71f7da149
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-01 19:57:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, year)
+    @roster[year] = [] if !@roster[year]
+    @roster[year] << name
+  end
+
+  def grade(year)
+    self.roster[year]
+  end
+
+  def sort
+    sorted = {}
+
+    @roster.each do |year, array_of_students|
+      sorted[year] = []
+      sorted[year] = array_of_students.sort
+    end
+    sorted
+
+  end
+
+end
