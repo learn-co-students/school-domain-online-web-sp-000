43201347a336017248e2d9e3cb00ec27609799ae
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-28 21:39:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+
+class School 
+  
+  def initialize(name)
+    @name= name
+    @roster= {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def name 
+    @name
+  end
+  
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+    @roster[grade] = []
+    @roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |grade, student_array|
+      student_array.sort!
+    end
+    @roster
+  end
+  
+  
+end
