ac96c53faaa33f81e753ebe299b7a52ef8f9a321
Evan Burton <evan@heyevan.com>
2020-04-30 06:23:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+@name = name
+@roster = {}
+end
+
+def add_student(name, grade)
+if roster[grade]
+  roster[grade]<<name
+else
+roster[grade] = [name]
+    end
+  end
+
+
+def grade(grade)
+roster[grade]
+end
+
+def sort
+  @roster.each do |grade, name|
+  name.sort!
+    end
+  end
+end
