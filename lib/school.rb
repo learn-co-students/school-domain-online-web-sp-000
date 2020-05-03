68d54323a9dd19dfeea1927987a9e85c64888625
Jake G <jake098877@gmail.com>
2020-05-03 22:16:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+
+  roster = {GRADES[0..12]}
+
+  def add_student=(name, grade)
+    @name = name
+    @grade = grade
+    roster[:grades].collect do |grades|
+      if grades == @grade
+        GRADES << @name
+      end
+    end
+  end
+end
