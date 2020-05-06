4323abf7426fb46684698ac41a924fb5643fcc1d
Jung Lee <lee.jung.s95@gmail.com>
2020-05-06 03:54:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+
+class School
+
+  def initialize(school_name)
+    @school = school_name
+    @roster = {}
+  end
+
+  def school_name
+    @school
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+    @add_student = @roster
+  end
+
+  def grade(grade)
+    @grade = @roster.each do |grades, students|
+      if grades == grade
+        return students
+      end
+    end
+    @grade
+  end
+
+  def sort
+    @roster.each do |grades, value|
+      value.sort!
+    end
+  end
+end
