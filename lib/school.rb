9c71b15aebb4d9944d377ad3ebe12aedfbd3471c
ems_codes <emilyxi@gmail.com>
2020-05-19 13:53:06 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+  
+    def grade(grade)
+        @roster.each {|keys, array| return array if keys == grade }
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each {|keys, array| sorted_roster[keys] = array.sort}
+        return sorted_roster
+    end
+
+end
---
+++ test.rb
@@ -0,0 +1,7 @@
+hash = {
+    10 => ["emily", "bryan"],
+    11 => ["sam", "tyler"]
+}
+
+hash.has_key?(10)
+hash[10] << "Bryan"
