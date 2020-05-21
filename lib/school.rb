14d48f97e7df7d6d272520405770f2cfe02b0385
Your Name <wkdewey@gmail.com>
2020-05-21 16:26:00 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+# code here!sda
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if !(@roster.keys.include?(grade))
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+        @roster
+    end
+
+end
