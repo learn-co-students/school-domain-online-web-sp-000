2874f88ea75c15e258761c9fd69c8b29ab219a59
Martin Achtermann <mdacht777@gmail.com>
2020-06-15 04:15:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  def add_student(name,grade)
+    if @roster[grade]
+      @roster[grade]<<name
+    else
+      @roster[grade]=[name]
+    end
+  end
+  def grade(arg)
+    @roster[arg]
+  end
+  def sort
+    @roster.each do |grade,name|
+      name.sort!
+    end
+  end
+end
