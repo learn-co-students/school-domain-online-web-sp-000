c2444b635ca33531c17d51b8222f75fee8781021
Seth Near <seth.j.near@gmail.com>
2020-06-22 05:40:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+attr_reader :name
+
+def add_student(name, grade)
+  if @roster.keys.include?(grade)
+    @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+end
+
+def grade(grade)
+  return @roster[grade]
+end
+
+end
