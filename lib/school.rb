cb66e4b299cad3642236186eaa420b5178f198c3
Martin Moy <martinmoy1991@gmail.com>
2020-07-19 05:40:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,17 @@
 # code here!
 class School
   attr_accessor :name, :roster
+
   def initialize(name)
-    @name = name
+    @name  = name
     @roster = Hash.new { |name, grade| name[grade] = []}
   end
 
   def add_student(name, grade)
-    @roster[grade] << name
-  end
+   @roster[grade] << name
+ end
 
-  def grade(grade)
+ def grade(grade)
     @roster[grade]
   end
 
