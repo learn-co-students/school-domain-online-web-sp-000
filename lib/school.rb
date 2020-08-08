99508ae416f56ab0b8a729ec6d6f7f6f8def50d1
Morgan Matney <morgan.matney89@gmail.com>
2020-08-08 14:13:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,21 +7,17 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster.include?(grade)
-      @roster[grade] << name
-    else
-      @roster[grade] = []
-      @roster[grade] << name
-    end
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
     sorted = {}
-    @roster.each do |grade, names|
+    roster.each do |grade, names|
       sorted[grade] = names.sort
     end
     sorted
