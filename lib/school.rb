29c1583a081580186944fe0705a01c8c76704d87
Drew Thompson <drewallynthompson@gmail.com>
2020-09-03 00:26:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School 
+  
+attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade]||roster[grade] = []
+    roster[grade] << student 
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sorted_hash = {}
+    @roster.each do |grade, student| 
+      sorted_hash[grade] = student.sort 
+    end
+    sorted_hash
+  end
+
+end
