2cd90a0afaaaec2a2d1d6b395de9461c3847b4ab
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-17 23:58:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :student_grade
+  roster = {}
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(student_name, student_grade)
+    if roster[student_grade] == nil
+      roster[student_grade] = []
+    end
+    roster[student_grade] << "#{student_name}"
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    roster.map { |grade, names| roster[grade] = names.sort }
+    roster
+  end
+
+
+end
