2ee3bef6a3798c86f60bb046bfb616b1d10eed94
Jc Dior <jessicaclairdior@gmail.com>
2020-09-17 00:11:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+def add_student(name, grade)
+  if @roster.include?(grade)
+  @roster[grade] << name
+else
+  @roster[grade] = [name]
+end
+end
+
+def grade(grade)
+  self.roster[grade]
+end
+
+def sort
+  self.roster.each do |grade, students|
+    students.sort!
+  end
+end
+
+end
