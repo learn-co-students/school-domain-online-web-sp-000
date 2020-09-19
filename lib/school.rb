ed7d9b5b22852d0d592d9b37bf2726507676c40a
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-19 23:09:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
+  
+
+# Here's learn.co's solution:
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
