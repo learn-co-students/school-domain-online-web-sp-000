d14b089e69b6ba15f02a8ca8719a59680514235a
Christerle Nounoute <christerlesn@gmail.com>
2020-09-26 04:40:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
+
+  def grade(level)
+    roster.detect do |x, y|
+      if x == level
+        return y
+      end
+    end
+  end
+
+
+
+  def sort
+    new_hash = {}
+    roster.each do |student, grade|
+      new_hash[student] = grade.sort
+    end
+    new_hash
+    end
+
+end
