5adfde74b79e344821895417698f01383a12b4b0
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-30 21:30:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,67 @@
-# code here!
+# # code here!
+# class School
+  
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+
+#   def roster
+#     @roster
+#   end
+ 
+#   def add_student(name, grade)
+#     if roster.keys.include?(grade)
+#       roster[grade] << name
+#     else
+#       roster[grade] = []
+#       roster[grade] << name
+#     end
+#   end
+ 
+#   def grade(grade)
+#     roster[grade]
+#   end
+
+#   def sort
+#     roster.each do |key, value|
+#       roster[key] = value.sort!
+#     end
+#   end
+
+# end
+
+
 class School
+  
+  attr_accessor :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
-
-  def roster
-    @roster
-  end
- 
+  
+  # def add_student(name, grade)
+  #   if roster[grade]
+  #     roster[grade] << name
+  #   else
+  #     roster[grade] = []
+  #     roster[grade] << name
+  #   end
+  # end
+  
   def add_student(name, grade)
-    if roster.keys.include?(grade)
-      roster[grade] << name
-    else
-      roster[grade] = []
-      roster[grade] << name
-    end
+    roster[grade] ||= []
+    roster[grade] << name
   end
- 
+  
   def grade(grade)
     roster[grade]
   end
-
+  
   def sort
-    roster.each do |key, value|
-      roster[key] = value.sort!
-    end
+    roster.each{|key, value| value.sort!}
   end
-
+  
 end
+
