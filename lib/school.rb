78d9950c99d0e9cb1381b1747c629aebacf1da30
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-02 18:08:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+# code Here
+class School
+
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+  roster[grade] ||= []
+  roster[grade] << name
+end
+
+def grade(grade)
+  return roster[grade]
+end
+
+def sort
+  new_hash = {}
+  roster.each do |first_name, last_name|
+    new_hash[first_name] = last_name.sort
+end
+return new_hash
+end
+end
