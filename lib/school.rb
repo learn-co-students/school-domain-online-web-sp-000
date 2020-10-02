e014c483d8ea9aee1a9ce60274fdcae8a74b7806
Rosie Choi <rosiechoimanso@gmail.com>
2020-10-02 19:58:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School 
+  
+  attr_reader :school, :roster
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+  if roster.include?(grade)
+    roster[grade] << name
+  else
+  roster[grade]=[]
+  roster[grade] << name
+end
+end
+
+def grade(grade)
+roster[grade]
+end
+
+def sort 
+  sorted={}
+  roster.each do |grade,name|
+    sorted[grade]=name.sort
+  end
+  sorted
+end
+
+end
+
+
+
