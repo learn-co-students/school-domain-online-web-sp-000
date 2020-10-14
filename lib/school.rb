75149c3ce98918f36c370c19fd390c62fd59e3f2
fut0k0 <cccabell@icloud.com>
2020-10-14 14:29:22 -0700

    Passing lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+	attr_reader :roster
+
+	def initialize(name)
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		roster[grade] ? roster[grade].push(name) : roster[grade] = [name]
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.each {|grade, students| students.sort!}
+	end
+end
