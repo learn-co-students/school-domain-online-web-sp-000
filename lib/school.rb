1c0d66a9cd42fb2a2db4a4f10ea4468f5e9ed1bc
Marc Steele <marcsteele2014@gmail.com>
2020-10-20 14:32:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+attr_accessor
+attr_reader :name, :roster, :grade
+attr_writer
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+  if @roster[grade].is_a?(Array)
+      @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+end
+
+# def grade
+#
+# end
+
+end
