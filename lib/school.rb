f5871d52931e73835793710b56f0c3fb001381d0
Chris Benedict <cabx88@gmail.com>
2020-10-22 07:39:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @roster = Hash.new
+  end
+  
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+  
+end
