30cc9ae89b4fb28a8dade9cb1af1e5f200b0d9b1
Alison Williams <apw5011@gmail.com>
2020-11-20 01:59:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+  
+  attr_reader :name, :roster, :grade 
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name 
+    else 
+      @roster[grade] = []
+      @roster[grade] << name 
+    end
+  end
+  
+  def grade(grade_num)
+    @roster.each do |grade, name|
+      if grade_num == grade
+      return name
+      end
+    end 
+  end 
+  
+  def sort
+    @roster.each do |grade, name|
+      name.sort! 
+    end 
+  end
+  
+end 
