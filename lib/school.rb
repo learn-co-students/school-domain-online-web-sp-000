3b2bf09cc71023290c23f3ebcd2751137eedc46b
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 07:49:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      roster[grade].push(name)
+    else 
+      roster[grade] = []
+      roster[grade].push(name)
+    end 
+  end
+  
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
