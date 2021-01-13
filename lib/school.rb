9cadd183eeeff8eb1dec75e15b7bde4869665b10
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-13 21:53:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
 # code here!
+
+  roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
