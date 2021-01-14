39bee77e01b10702d8f3b1d7d2dff2cd26696af7
Taylor Treece <taylor.treece@protonmail.com>
2021-01-14 14:54:40 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if roster.has_key?(grade) ? roster[grade] << name : roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, name|
+            sorted[grade] = name.sort
+        end
+        sorted
+    end
+end
