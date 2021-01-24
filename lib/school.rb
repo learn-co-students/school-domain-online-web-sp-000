5a649cfb774474c8a400f154d0cef57d247c248f
Chris Baum <christopher.baum@valpo.edu>
2021-01-24 01:46:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,28 @@
 require 'pry'
 class School
+  attr_reader :name, :roster
 
-  def initialize(school)
-    @school = school
-    @current_roster = {}
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|h, k| h[k] = []}
   end
 
   def roster
-    @current_roster
+    @roster
   end
 
   def add_student(name, grade)
-
+    self.roster[grade] << name
   end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, names|
+      names.sort!
+    end
+  end
+
 end
