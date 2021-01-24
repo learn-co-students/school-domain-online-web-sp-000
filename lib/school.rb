498ab6835e6ef04cdda44d2f85a8a674c23ddf03
Chris Baum <christopher.baum@valpo.edu>
2021-01-24 00:27:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+require 'pry'
+class School
+
+  def initialize(school)
+    @school = school
+    @current_roster = {}
+  end
+
+  def roster
+    @current_roster
+  end
+
+  def add_student(name, grade)
+
+  end
+end
