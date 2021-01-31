3f3b02b19ac1aa15fc9d807d60d5153a6d3aedc9
Mike Nanto <mike@thecowboymechanic.com>
2021-01-31 18:22:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  attr_accessor :name, :roster    
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+  
+  def grade(school_grade)
+    roster[school_grade]
+  end
+  
+  def sort 
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
