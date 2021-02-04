5f59425a3c4c501bd63993f08fdfcb193b3b096b
Keginold Philip <keginoldp@hotmail.com>
2021-02-04 17:30:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+	class School
+
+		attr_accessor :roster, :name
+
+		def initialize(name)
+			@name = name
+			@roster = {}
+		end
+
+		def add_student(name, grade)
+			if @roster.include?(grade)
+				@roster[grade] << name
+			else
+				@roster[grade] = []
+				@roster[grade] << name
+		end
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.each do |grade, student|
+			student.sort!
+
+	end
+end
+end
+	school = School.new("Bayside High School")
