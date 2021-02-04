5511c4be7e57f1daaa5a376adbfd54ceb54c655e
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-02-04 19:30:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :school, :roster, :grade, :name
+  
+  
+  def initialize(roster)
+    @roster = Hash.new {|h,k| h[k] = []}
+  end 
+  
+  
+ def add_student(name, grade)
+   @name = name
+   @grade = grade 
+   roster[grade] << name 
+  end 
+   
+  def grade(grade)
+    @roster[grade]
+  end 
+
+  def sort 
+   @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end 
+  end 
+  
+end 
+
