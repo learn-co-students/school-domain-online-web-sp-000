87c3f6e0963a26a772807808f5d1cf0770f99ad2
Yiliang Chen <yiliangchen0113@gmail.com>
2021-02-12 23:36:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+  
+  attr_accessor :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new
+  end
+  
+  def add_student(student, year)
+    roster[year] ||= []
+    roster[year] << student
+  end 
+  
+  def grade(year)
+    roster.fetch(year) if roster.has_key?(year)
+  end 
+  
+  def sort 
+    roster.sort_by.map {|k, v| [k, v.sort]}.to_h
+  end 
+  
+end 
+
