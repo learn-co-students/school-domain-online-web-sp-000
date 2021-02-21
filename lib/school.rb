d7e5d589070defd69c6fa9320f165bdda63212b5
Jouse then <jthen93@gmail.com>
2021-02-21 23:42:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    self.roster = {}
+  
+  end
+  
+  def add_student (sname, sgrade)
+    if self.roster.has_key?(sgrade) == false
+      self.roster[sgrade] = [] 
+    end
+    self.roster[sgrade] << sname
+  end
+  
+  def grade(g)
+    return @roster[g]
+  end
+  
+  def sort 
+    @roster.each do |g, students|
+      students.sort!
+    end
+  end
+end
