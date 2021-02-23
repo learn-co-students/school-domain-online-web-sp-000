007251f5a6b44bd7b475422ea11a4fd1c59501eb
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-23 22:42:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if !@roster.include?(grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = @roster.transform_values{|students| students.sort}
+
+    sorted
+
+  end
+
+
+end
