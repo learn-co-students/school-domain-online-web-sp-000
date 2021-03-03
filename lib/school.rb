9da7964fa3e5654d7c33fcaf6cf07c8ed1807e49
Devorah Posner <dleahposner@gmail.com>
2021-03-03 00:06:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School 
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def roster
+    @roster 
+  end 
+  
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    
+  end 
+    
+    
+end 
