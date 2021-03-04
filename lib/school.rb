4a2f8dbf801265d8a83fef9da20cc9a25b568f8c
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-04 02:00:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+class School
+attr_reader :school, :roster
+
+def initialize(school, roster = {})
+  @school = school
+  @roster = roster
+end
+
+def add_student(name, grade)
+  @roster[grade] = [] unless @roster.include? grade
+  @roster[grade] << name
+end
+
+def grade(grade)
+  @roster[grade] if @roster.include?(grade)
+end
+
+def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+end
+
+end
+
+school = School.new("TF South")
+school.add_student("Matt", 10)
+school.add_student("Tim", 10)
+school.add_student("Zach", 10)
+school.add_student("Alex", 10)
+school.add_student("Mochi", 11)
