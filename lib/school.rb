9b5bb033ead8ee92828756361f8c0ed2a7872d2c
Tim Quimbo <tquimbo1@gmail.com>
2021-03-09 00:15:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+attr_accessor :school, :roster, :student, :grade
+
+
+
+def initialize(roster)
+  @roster= {}
+end
+
+def add_student(student, grade)
+#insert additonal values for key into an array
+roster[grade] ||= []
+roster[grade] << student
+
+end
+
+def grade(grade)
+return roster[grade]
+end
+
+def sort
+sorted = {}
+roster.each do |grade, students|
+sorted[grade] = students.sort
+end
+return sorted
+end
+
+end
