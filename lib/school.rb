247918f644798e0582f87bd5f7a594ee329a69bb
Guosa_Edebiri <gedebiri@gmail.com>
2021-04-07 04:57:48 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort_by! {|w| w.downcase}
+        end.sort.to_h
+    end
+
+end
