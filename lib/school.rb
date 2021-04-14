e0afa23f3e483a7b4b855dd6cb4c59a6025a52d3
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-14 22:27:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,35 @@
 require "pry"
 
 class School 
-  attr_reader :name :roster
+  attr_reader :name, :roster
   
   def initialize(name)
     @name= name
-    
-    
     @roster= {}
-  
-  
- 
   end 
   
   def add_student(student_name, grade)
-    roster[student_name] = grade
+    if @roster.keys.include?(grade)
+      @roster[grade] << student_name  
+    else   
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
   end 
   
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    @roster.collect do |grade,students|
+      students.sort { |a,b| a<=>b} 
+    end 
+  end 
+    #binding.pry 
+  #end   
+   
 end 
 
-
-school=School.new("New")
-binding.pry
+#school=School.new("New")
+#binding.pry
