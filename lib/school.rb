c9d89f46ffd2fa1d61bff1d5ee4b9a48a38d28d8
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-14 17:20:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require "pry"
+
+class School 
+  attr_reader :name :roster
+  
+  def initialize(name)
+    @name= name
+    
+    
+    @roster= {}
+  
+  
+ 
+  end 
+  
+  def add_student(student_name, grade)
+    roster[student_name] = grade
+  end 
+  
+end 
+
+
+school=School.new("New")
+binding.pry
