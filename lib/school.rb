5a99158245843c7037ffd701430b9fdde3601000
Drew Keat <thedrewkeat@gmail.com>
2021-04-17 11:59:33 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+require 'pry'
+class School
+    attr_reader :name
+    attr_reader :roster
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(sname, grade)
+        @roster.keys.include?(grade) ? @roster[grade] << sname : @roster[grade] = [sname]
+    end
+
+    def grade(key)
+        @roster[key]
+    end
+
+    def sort
+        @roster.sort.map{|g,s| [g, s.sort]}.to_h
+    end
+end
