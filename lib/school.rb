745643c151cb7b64e9c3eaff4358089b3eb8726a
Harrison Price <hprice518@gmail.com>
2021-04-25 01:45:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,10 +9,31 @@ attr_accessor :name, :roster
   end 
   
   def add_student(name, grade)
-    roster[grade] = [name]
-    roster.each do |name|
-      roster[:name]
+    
+    if roster.has_key?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = [name]
     end
+    
+  end
+  
+  def grade(grade)
+    roster[grade]
   end
 
+  def sort 
+    roster.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
 end
+  end 
+
+end
+
+
