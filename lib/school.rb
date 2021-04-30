3198d23f99ca8f0c77ec422658976152fe6dcf15
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-30 22:22:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+    def roster
+      @roster
+    end
+
+    def add_student(name, grade)
+      @roster[grade] ||= []
+      @roster[grade] << name
+    end
+
+    def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
