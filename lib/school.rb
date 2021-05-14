d7a882c3ab909a5bb12f0f35e37ed823a9ec95b4
Codi Kraus <codi.kraus@gmail.com>
2021-05-14 01:14:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, student_name|
+      student_name.sort!
+    end
+    roster.sort.to_h
+  end
+end
