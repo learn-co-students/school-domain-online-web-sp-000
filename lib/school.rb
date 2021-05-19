a1eaedfc9c3636c04131331f3614e5426863305a
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-19 17:06:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster.fetch(grade)
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      name.sort!
+    end
+  end
+end
