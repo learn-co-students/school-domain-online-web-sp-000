0c3dd853b09bfd970c6be68dc3207f80639902db
Lj <tnedutsl457892@gmail.com>
2021-06-28 09:22:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if(roster[grade])
+            roster[grade] << name
+        else
+            roster[grade] = []
+            roster[grade] << name
+        end
+        roster
+        # binding.pry
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+
+end
